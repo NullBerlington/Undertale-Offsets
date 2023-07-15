c781d09f0f189c847c732bb665416947a89bc8c6
NullBerlington/NullAnaylsis <118641414+NullBerlington@users.noreply.github.com>
2023-07-14 20:58:55 -0400

    Create OffsetDump.cpp
    Offsets for undertale

---
+++ OffsetDump.cpp
@@ -0,0 +1,6 @@
+// Offsets for undertale
+constexpr ptrdiff_t health = 0x006626cc;
+constexpr ptrdiff_t blue_soul = 0x0066831c;
+constexpr ptrdiff_t green_soul = 0x0066834d;
+constexpr ptrdiff_t red_soul = 0x00668360;
+constexpr ptrdiff_t dialog = 0x0065dbe8;
